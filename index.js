b80322407b7b485f425389a206366ba6b6d156a5
og-JND <69082025+og-JND@users.noreply.github.com>
2023-08-11 14:43:33 -0700

    Second commit. Almost done w the app, just have to add fucntionality to complete tasks

--- index.js
+++ index.js
@@ -9,12 +9,43 @@ const port = 3000;
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const visitorLogStream = fs.createWriteStream(`${__dirname}/access.log`, { flags: 'a' })
+let personalList = [];
+let workList = [];
+
 
 app.use(morgan("combined", {stream: visitorLogStream}));
 app.use(bodyParser.urlencoded({extended:true}));
 
+// function (params) {
+    
+// }
+
 app.get("/", (req, res)=>{
-    res.render("index.ejs")
+    res.render("index.ejs");
+})
+
+app.get("/work", (req, res)=>{
+    res.render("work.ejs");
+})
+
+app.post("/workSubmit", (req, res)=>{
+    let submittedTask = req.body.task;
+    let capitallizedSubmittedTask = submittedTask.charAt(0).toUpperCase() + submittedTask.slice(1)
+    // res.render("work.ejs");
+    workList.push(capitallizedSubmittedTask);
+    res.render("work.ejs", {
+        workList: workList
+    })
+})
+
+app.post("/submit", (req, res)=>{
+    let submittedTask = req.body.task;
+    let capitallizedSubmittedTask = submittedTask.charAt(0).toUpperCase() + submittedTask.slice(1)
+    // res.render("work.ejs");
+    personalList.push(capitallizedSubmittedTask);
+    res.render("index.ejs", {
+        personalList: personalList
+    })
 })
 
 app.listen(port, (req, res)=>{
